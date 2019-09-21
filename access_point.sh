04b54e58161837175cec28920920ced19144fa42
Tatsuya Ishikawa <tatsuyai713@gmail.com>
2019-09-22 00:57:51 +0900

    add access point setting

---
+++ access_point.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+SUBNET=192.168.0.0
+NETMASK=255.255.255.0
+AP_ADDR=192.168.0.1
+WIFI_IF=wlp61s0
+
+
+start() {
+    service network-manager stop
+    sleep 1
+    ifconfig ${WIFI_IF} ${AP_ADDR} netmask ${NETMASK} up
+    sleep 1
+    service hostapd start
+    if [ $? != 0 ]; then
+        echo "failed to start hostapd."
+        return 1
+    fi
+
+    echo "start access-point."
+    return 0
+}
+
+stop() {
+    service hostapd stop
+    sleep 1    
+    ifconfig ${WIFI_IF} down
+    sleep 1
+    service network-manager start
+
+    echo "stop access-point."
+    return 0
+}
+
+case $1 in
+"start" )
+    start
+    ;;
+
+"stop" )
+    stop
+    ;;
+
+*)
+    echo "usage: $0 start|stop"
+    exit 1
+esac
